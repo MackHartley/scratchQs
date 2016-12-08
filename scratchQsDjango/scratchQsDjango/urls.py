28964b39ce840a04ebded4b00f65d7d8a03e070c
qisheng <qli@macalester.edu>
2016-12-08 14:27:41 -0600

    css and links work!

--- scratchQsDjango/scratchQsDjango/urls.py
+++ scratchQsDjango/scratchQsDjango/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     url(r'^scratchQs/$', scratchq_views.index, name="index"),
     url(r'^scratchQs/(?P<question_id>[0-9]+)/$', scratchq_views.answers, name="answers"),
     url(r'^scratchQs/signup', scratchq_views.signup, name="signup"),
-    url(r'^scratchQs/answer/$', scratchq_views.answer_page, name="answer_page") #not working yet
+    #url(r'^scratchQs/answer/$', scratchq_views.answer_page, name="answer_page") #not working yet
 ]
