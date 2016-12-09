2a551dfb235e7cfd0d937b433d85e50069f30eb1
MackHartley <mhartley@macalester.edu>
2016-12-08 20:53:56 -0600

    added ajax content

--- scratchQsDjango/scratchQs/templates/scratchQs/ajax.js
+++ scratchQsDjango/scratchQs/templates/scratchQs/ajax.js
@@ -1,12 +1,24 @@
 function submitQuestion(){
-	var text = $("#question-field").val()
+	var title = $("#question-title").val()
+	var content = $("#question-content").val()
+	var category = $("#question-category").val()
 	$.ajax({
 		type: "POST"
 		url: "../add_question/"
-		data: {"question": text}
+		data: { "title": title,
+				"content": content
+				"category": category}
 		datatype: "json",
 		success: function(response) {
 			location.reload()
 		}
 	});
 }
+
+'''
+NEED TO ADD IN CATEGORIES
+
+need a category field in ask a new questions
+
+need to populate categories in the left hand divider
+'''
