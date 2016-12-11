eb37aaabba367687a8abc4d44e7ad6055c01930c
qisheng <qli@macalester.edu>
2016-12-10 19:33:20 -0600

    change to views

--- scratchQsDjango/scratchQs/views.py
+++ scratchQsDjango/scratchQs/views.py
@@ -4,45 +4,46 @@ from .models import Answer, Question
 import json
 from django.template import loader
 
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.views import generic
+
 
 # Create your views here.
 
-# class Question(models.Model):
-# 	title = models.CharField(max_length=200, unique=True)
-# 	content = models.TextField()
-# 	category = models.charField(max_length=100)
-# 	votes = models.IntegerField(default=0)
-# 	def __str__(self):
-# 		return 'question: %s' % (self.title)
+class IndexView(generic.ListView):
+    template_name = 'scratchQs/index.html'
+    context_object_name = 'questions'
 
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.all()
 
-#class Answer(models.Model):
-#	content = models.TextField()
-#	votes = models.IntegerField(default=0)
+# def index(request):
+# 	#pre load data
+# 	q1 = Question(title="title 1", content="content 1", pk=0)
+# 	q2 = Question(title="title 2", content="content 2", pk=1)
+# 	q1.save()
+# 	q2.save()
+# 	context = {Question.objects.all()}
+# 	# print(len(questions))
+# 	# questionList = []
+# 	# questionList.append(q1)
+# 	# questionList.append(q2)
+# 	# context = {"questions" : questionList}
+# 	# context = 
+# 	# print(context)
+# 	# questionList = []
+# 	# questions = Question.objects.all()
+# 	# print(len(questions))
+# 	# for question in questions:
 
-def index(request):
-	#pre load data
-	q1 = Question(title="title 1", content="content 1", pk=0)
-	q2 = Question(title="title 2", content="content 2", pk=1)
-	q1.save()
-	q2.save()
-	questions = Question.objects.all()
-	print(len(questions))
-	questionList = []
-	questionList.append(q1)
-	questionList.append(q2)
-	context = {"questions" : questionList}
-	print(context)
-	# questionList = []
-	# questions = Question.objects.all()
-	# print(len(questions))
-	# for question in questions:
-
-	# 	question_context = {"title":question.title, "content": question.content, "votes":question.votes,
-	# 		"category": question.category, "id" : question.pk}
-	# 	questionList.append(question_context)
-	# context = {"questions" : questionList}
-	return render(request, "scratchQs/index.html", context)
+# 	# 	question_context = {"title":question.title, "content": question.content, "votes":question.votes,
+# 	# 		"category": question.category, "id" : question.pk}
+# 	# 	questionList.append(question_context)
+# 	# context = {"questions" : questionList}
+# 	return render(request, "scratchQs/index.html", context)
 
 # def answer(request,question_id):
 # 	question = Question.objects.get(pk=question_id)
